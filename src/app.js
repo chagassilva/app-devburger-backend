6c717f7d11ad4e3fcf97dcb3c2711119f0bbaaaf
chagassilva <chagassilvati@gmail.com>
2025-05-30 10:47:25 -0300

    Atualizando o app.js

--- src/app.js
+++ src/app.js
@@ -14,14 +14,17 @@ const corsOptions = {
 
 class App {
   constructor() {
-    this.app = express();
-    this.app.use(cors(corsOptions));
+    this.app = express();   
     this.middlewares();
     this.routes();
   }
 
 
     middlewares() {
+
+        this.app.use(cors(corsOptions));
+        this.app.options('*', cors(corsOptions));
+
         this.app.use(express.json());
         this.app.use('/product-file', express.static(resolve(__dirname, '..','uploads')));
         this.app.use('/category-file', express.static(resolve(__dirname, '..','uploads')));
