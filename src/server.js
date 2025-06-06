1822dd3a1c2ab4599d33c42c33735c0e9c811106
chagassilva <chagassilvati@gmail.com>
2025-06-05 22:00:54 -0300

    Atualizacao do servidor4

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@
 import app from './app';
 
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.listen(port,"0.0.0.0", () => {
   console.log(`Server started on port ${port}`);
